a5236f8df252ed0a19d3e3dda7aa3cd38b67cadd
gustavodetoni <gustavodetoni2407@gmail.com>
2024-10-01 18:59:42 -0300

    feat: dockerfile

--- backend/server/db.js
+++ backend/server/db.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB conectado');
   } catch (err) {
     console.error('Erro na conexão com o MongoDB:', err.message);
     process.exit(1); 
