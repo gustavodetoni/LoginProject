94ccc8e564275e4c9bda5f097c982ceb3125e45b
gustavodetoni <gustavodetoni2407@gmail.com>
2024-10-06 20:39:51 -0300

    feat: resetpassword submit

--- frontend/src/components/ForgotPasswordForm.vue
+++ frontend/src/components/ForgotPasswordForm.vue
@@ -3,12 +3,13 @@
     <h2>Esqueceu a Senha</h2>
     <form @submit.prevent="handleSubmit">
       <div class="form-group">
-        <p>Digite o seu email de login</p>
+        <h4>Digite o seu email de login</h4>
         <p>Você receberá um email com um Token que sera validado para redefinição de senha</p>
         <input id="email" v-model="email" type="email" placeholder="Seu email" required>
+        <p></p>
         <router-link to="/login">Voltar</router-link>
       </div>
-      <button type="submit" class="btn" @click="goToResetPassword">Enviar</button>
+      <button type="submit" class="btn">Enviar</button>
     </form>
   </div>
 </template>
@@ -18,6 +19,7 @@ import { ref } from 'vue'
 import { useRouter } from 'vue-router'
 
 const email = ref('')
+const router = useRouter();
 
 const handleSubmit = async () => {
   try {
@@ -30,16 +32,15 @@ const handleSubmit = async () => {
     });
 
     if (!response.ok) {
-      alert('Erro ao enviar email');
+      alert('Email não corresponde a nenhuma conta');
+    } else {
+      router.push('/reset-password')
     }
   } catch (error) {
     console.error('Erro no servidor:', error);
   }
 }
 
-const goToResetPassword = () => {
-  router.push('/reset-password')
-}
 </script>
 
 <style scoped>
--- frontend/src/components/ResetPasswordForm.vue
+++ frontend/src/components/ResetPasswordForm.vue
@@ -1,39 +1,81 @@
 <template>
   <div>
-    <h2>Redefinir Senha</h2>
+    <h2 v-if="!isTokenValid">Validação de Token</h2>
+    <h2 v-if="isTokenValid">Redefinir Senha</h2>
+    
     <form @submit.prevent="handleSubmit">
-      <div class="form-group">
+      <div class="form-group" v-if="!isTokenValid">
+        <label for="token">Código de Validação</label>
+        <input id="token" v-model="token" type="text" maxlength="6" required placeholder="Digite o código de 6 dígitos">
+      </div>
+
+      <div class="form-group" v-if="isTokenValid">
         <label for="newPassword">Nova senha</label>
         <input id="newPassword" v-model="newPassword" type="password" placeholder="Digite a nova senha" required>
         <p></p>
-        <label for="newPassword">Digite novamente a senha</label>
-        <input id="newPassword" v-model="newPassword" type="password" placeholder="Digite a nova senha" required>
+        <label for="confirmPassword">Repita a senha</label>
+        <input id="confirmPassword" v-model="confirmPassword" type="password" placeholder="Repita a nova senha" required>
       </div>
-      <button type="submit" class="btn">Redefinir Senha</button>
+      
+      <button type="submit" class="btn">{{ isTokenValid ? 'Redefinir Senha' : 'Validar Código' }}</button>
     </form>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
-const newPassword = ref('')
+const router = useRouter();
+const token = ref('');
+const newPassword = ref('');
+const confirmPassword = ref('');
+const isTokenValid = ref(false);
 
 const handleSubmit = async () => {
-  try {
-    const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/auth/reset-password`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ newPassword: newPassword.value }),
-    });
+  if (!isTokenValid.value) {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/auth/validate-token`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ resetToken: token.value }),
+      });
 
-    if (!response.ok) {
-      alert('Erro ao redefinir senha');
+      if (!response.ok) {
+        alert('Código de validação inválido');
+        return;
+      }
+
+      isTokenValid.value = true;
+    } catch (error) {
+      console.error('Erro no servidor:', error);
     }
-  } catch (error) {
-    console.error('Erro no servidor:', error);
+  } else {
+    if (newPassword.value !== confirmPassword.value) {
+      alert('As senhas não coincidem');
+      return;
+    }
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/auth/reset-password`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ resetToken: token.value, newPassword: newPassword.value }),
+      });
+
+      if (!response.ok) {
+        alert('Erro ao redefinir senha');
+        return;
+      }
+      alert('Senha redefinida com sucesso');
+      router.push('/login'); 
+    } catch (error) {
+      console.error('Erro no servidor:', error);
+    }
   }
 }
 </script>
